efe49371b8151b19daec2fb94cdf8e2b9285d588
saqib867 <saqibhussain9910@gmail.com>
2023-11-25 19:43:14 +0500

    changes made

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 
 const app = express()
+const corsOptions = {
+    origin: ['https://desol-frontend-two.vercel.app','http://localhost:3000'],
+    allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Methods", "Access-Control-Request-Headers"],
+    credentials: true,
+    enablePreflight: true
+}
 
-app.use(cors());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions))
 // database connection
 app.use(express.json())     //hgkhghj
 app.use(bodyParser.json())
@@ -28,7 +30,6 @@ connection.then(()=>{
         console.log("error while connecting database",err)
 })
 
-app.options('*', cors())
 
 app.use('/api',authrouter)
 app.use('/api',carRouter)  
