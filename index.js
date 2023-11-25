a537c2eac784352d2dcb96996ddad5c39d019cb1
saqib867 <saqibhussain9910@gmail.com>
2023-11-25 12:13:59 +0500

    changes made in server

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/api',authrouter)
 app.use('/api',carRouter)  
 
 
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 app.listen(PORT,()=>{
        console.log("server is running on port ",PORT)
 })
